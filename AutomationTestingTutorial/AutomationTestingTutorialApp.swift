bcbcc2f2bac8d35e9b025cfef206bf0a3bcd4075
Chelsea <chelsea@tripwiretech.com>
2023-09-08 08:05:39 +1200

    Initial Commit

---
+++ AutomationTestingTutorial/AutomationTestingTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutomationTestingTutorialApp.swift
+//  AutomationTestingTutorial
+//
+//  Created by Charles Suddens on 8/09/23.
+//
+
+import SwiftUI
+
+@main
+struct AutomationTestingTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
